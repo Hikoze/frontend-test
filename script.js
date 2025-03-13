a13306cabc044545ca67be81cf9a1d0e8c470b39
Hikoze <yuzhong384@gmail.com>
2025-03-13 11:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('questionForm').addEventListener('submit', async (e) => 
     const name = document.getElementById('name').value;
     const question = document.getElementById('question').value;
 
-    const response = await fetch('https://5ee9da2c-685b-43e4-a9cf-1969dc061a76-00-x553vvpxu9eu.pike.replit.dev/', {
+    const response = await fetch('https://5ee9da2c-685b-43e4-a9cf-1969dc061a76-00-x553vvpxu9eu.pike.replit.dev/send-question', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, question })
